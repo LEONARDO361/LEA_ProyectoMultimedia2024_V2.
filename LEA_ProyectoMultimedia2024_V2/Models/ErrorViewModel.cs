2fdf197b97c7bcf41455a243f440e883bac59bd0
LeoSigmaLander <134085546+LEONARDO361@users.noreply.github.com>
2024-09-22 19:41:21 -0300

    Agregar archivos de proyecto.

---
+++ LEA_ProyectoMultimedia2024_V2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LEA_ProyectoMultimedia2024_V2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
